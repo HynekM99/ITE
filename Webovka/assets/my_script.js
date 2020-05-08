58ba0cbcf60e1a87e090d18cce6730cd8a243328
SagvanWeird <64409864+SagvanWeird@users.noreply.github.com>
2020-05-08 21:46:50 +0200

    Add files via upload

--- Webovka/assets/my_script.js
+++ Webovka/assets/my_script.js
@@ -1,6 +1,10 @@
 function on_loaded() {
     var address = "localhost:8881";
 
+    for (var key in teams) {
+        jsonData[teams[key]] = null;
+    }
+
     createButtons();
     setActiveButton();
     document.getElementById("header_img_link").setAttribute("href", "http://"+address)
@@ -12,16 +16,18 @@ function on_loaded() {
 }
 
 function createButtons() {
-    var btns = "";
+    var selectionBtns = "";
 
     for (var key in teams) {
         var team = teams[key];
         var capitalTeamName = team.charAt(0).toUpperCase() + team.slice(1);
-        var btn = "<button class=\"btn_team_select\" id=\""+team+"\" onclick=\"selectTeam(this.id)\">"+capitalTeamName+"</button>";
-        btns += "<div class=\"selection\" id=\"selection_"+team+"\"><div class=\"color_indicator\" id=\"color_indicator_"+team+"\" style=\"color:"+team+"\">"+teamStatusIndicators.offline+"</div><div class=\"btn_team_select\">"+btn+"</div></div>";
+        var btn = "<button class=\"btn_team_select_btn\" id=\""+team+"\" onclick=\"selectTeam(this.id)\">"+capitalTeamName+"</button>";
+        var indic = "<div class=\"color_indicator\" id=\"color_indicator_"+team+"\" style=\"color:"+team+"\">"+teamStatusIndicators.offline+"</div>";
+        var btnDiv = "<div class=\"btn_team_select\">"+btn+"</div>"
+        selectionBtns += "<div class=\"selection\" id=\"selection_"+team+"\">"+indic+btnDiv+"</div>";
     }
 
-    document.getElementById("team_select_menu").innerHTML = btns;
+    document.getElementById("team_select_menu").innerHTML = selectionBtns;
 }
 
 function setActiveButton() {
@@ -30,9 +36,8 @@ function setActiveButton() {
 }
 
 function setAllTeamStatuses() {
-    for (var key in teams) {
+    for (var key in teams)
         setTeamStatus(teams[key], jsonData[teams[key]] !== null)
-    }
 }
 
 function setTeamStatus(team, status) {
@@ -44,59 +49,55 @@ function selectTeam(btnID) {
     lastSelectedTeam = selectedTeam;
     selectedTeam = btnID;
     setActiveButton();
+    var dataAvailable = jsonData[selectedTeam] !== null;
+    setTableStatus(dataAvailable)
+    tableCreated = dataAvailable
+}
 
-    if (jsonData[selectedTeam] !== null) {
-        if (tableCreated) {
-            updateTable();
-        }
-        else {
-            createTable();
-        }
+function setTableStatus(visible) {
+    if (visible) {
         setNoStatsAlert("");
+        if (tableCreated)
+            updateTable();
+        else
+            createTable();
     }
     else {
         setNoStatsAlert("Nejsou k dispozici data");
-        document.getElementById("mainTable").remove();
-        tableCreated = false;
+        document.getElementById("main_table").remove();
     }
 }
 
 function createTable() {
-    var table = document.createElement("table");
-    table.setAttribute("id", "mainTable");
-    document.getElementById("stat_table").appendChild(table);
+    addNewElementToExisting("table", "main_table", "stat_table")
 
     for (var key in headers) {
-        var tr = document.createElement("tr");
-        var th = document.createElement("th");
-        var td = document.createElement("td");
-        tr.setAttribute("id", "tr_"+valueIndexes[key]);
-        th.setAttribute("id", "th_"+valueIndexes[key]);
-        td.setAttribute("id", "td_"+valueIndexes[key]);
-
-        document.getElementById("mainTable").appendChild(tr);
-        
-        document.getElementById("tr_"+valueIndexes[key]).appendChild(th);
-        document.getElementById("tr_"+valueIndexes[key]).appendChild(td);
+        addNewElementToExisting("tr", "tr_"+valueIndexes[key], "main_table");
+        addNewElementToExisting("th", "th_"+valueIndexes[key], "tr_"+valueIndexes[key]);
+        addNewElementToExisting("td", "td_"+valueIndexes[key], "tr_"+valueIndexes[key]);
 
         document.getElementById("th_"+valueIndexes[key]).innerHTML = headers[key];
-        document.getElementById("td_"+valueIndexes[key]).innerHTML = jsonData[selectedTeam][valueIndexes[key]];
-        if (valueIndexes[key] != "cas") {
-            document.getElementById("td_"+valueIndexes[key]).innerHTML += " °C";
-        }
+        updateTableValue("td_"+valueIndexes[key], jsonData[selectedTeam][valueIndexes[key]])
         tableCreated = true;
-        setAllTeamStatuses();
     }
+}
+
+function addNewElementToExisting(newElement, newElementId, existingElementId) {
+    var el = document.createElement(newElement);
+    el.setAttribute("id", newElementId);
+    document.getElementById(existingElementId).appendChild(el);
+}
+
+function updateTableValue(elementId, value) {
+    document.getElementById(elementId).innerHTML = value;
+    if (elementId != "cas" && value != "No data")
+        document.getElementById(elementId).innerHTML += " °C";
 }
 
 function updateTable() {
     for (var key in headers) {
-        document.getElementById("td_"+valueIndexes[key]).innerHTML = jsonData[selectedTeam][valueIndexes[key]];
-        if (valueIndexes[key] != "cas") {
-            document.getElementById("td_"+valueIndexes[key]).innerHTML += " °C";
-        }
+        updateTableValue("td_"+valueIndexes[key], jsonData[selectedTeam][valueIndexes[key]]);
     }
-    setAllTeamStatuses();
 }
 
 function setNoStatsAlert(message) {
@@ -119,20 +120,13 @@ function onSocketMessage(evt) {
     var jsonObject = JSON.parse(evt.data)
 
     for (var team in jsonData) {
-        if (jsonObject.hasOwnProperty(team)) {
+        var dataAvailable = jsonObject.hasOwnProperty(team);
+        if (dataAvailable)
             jsonData[team] = jsonObject[team];
-            if (!tableCreated) {
-                createTable();
-            }
-            else {
-                updateTable();
-            }
-            setNoStatsAlert("");
-        }
-        else {
-            setNoStatsAlert("Nejsou k dispozici data");
-        }
+        if (team == selectedTeam)
+            setTableStatus(dataAvailable);            
     }
+    setAllTeamStatuses();
 };
 
 function onSocketClose() {
